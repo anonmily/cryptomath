299d2fbe96cf4cbcad30a68cc31481d595fa050b
Michelle Liu <mliu@veloxwheels.com>
2020-07-13 14:51:37 -0700

    allow option to use element order

--- discrete_log_problem/baby_giant_step.py
+++ discrete_log_problem/baby_giant_step.py
@@ -2,17 +2,23 @@ import math
 import time
 from modarithmetic import get_mod_inverse, xgcd
 
-def babygiantstep(p,g,h, verbose=False, generate_all=False):
+def babygiantstep(p,g,h, use_element_order=False, verbose=False, generate_all=False):
     start_time = time.time()
 
-    # get the order m = p - 1
-    N = int(math.ceil(math.sqrt(p - 1)))
-    
+    if not use_element_order:
+        # get the order m = p - 1
+        N = p - 1
+    else:
+        N = int(Mod(g,p).multiplicative_order())
+     if verbose: print('N=',N)
+
+    n = int(math.ceil(math.sqrt(N)))
+
     # get mod inverse of g
     g_inv = get_mod_inverse(g, p)
 
     # Compute g^−n
-    g_inv_n = pow(g_inv, N, p)
+    g_inv_n = pow(g_inv, n, p)
 
     s = 1
     t = h
@@ -25,23 +31,27 @@ def babygiantstep(p,g,h, verbose=False, generate_all=False):
     if verbose:
         print('n=', N, 'g_inv=', g_inv,'g_inv_n=', g_inv_n)
 
-    for i in range(N):
-        
+    i = 0
+    while i < n:
+
         # Save babystep {value => index}
         tbl[s] = i
 
-        if verbose: print(f'i={i}, s={s}, t={t}')
+        # print('i=%s, s=%s, t=%s' % (i,s,t))
 
         # Check if giantstep is in table
         if t in tbl:
             j = tbl[t]
             if verbose: print('found t=', t, 'i=', i, 'j=', j)
-            x = i * N + j
+            x = i * n + j
             if not generate_all: break
-        
+
         # Increment values
         s = s*g % p
         t = (t * g_inv_n) % p
+
+        # Increment counter
+        i = i + 1
 
     end_time = time.time()
     if verbose: print('Elapsed duration: ', int(end_time - start_time), 'seconds')
